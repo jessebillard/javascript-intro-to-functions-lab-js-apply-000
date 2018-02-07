ccdf996439b3c3fea940e41f700db3f733bce207
Jesse Billard <jessebillard@mac.com>
2018-02-07 15:40:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === lowercase) {
+  string = 'hello'
+  if (string === lowercase) {
     return "I can't hear you!";
-  } else if (string.toUpperCase() === uppercase) {
+  } else if (string === uppercase) {
     return "YES INDEED!"
   } else if (string === 'I love you, Grandma') {
     return 'I love you, too.'
